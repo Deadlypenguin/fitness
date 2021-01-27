fb2f68c334d239a0fe74da62f1ebe6a4892e8293
Patrick Connelly <patrick@deadlypenguin.com>
2021-01-27 11:38:43 -0500

    Fixing lint issue with reserved name conflict

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const stravaRoute = require('./strava');
-const package = require('../../package.json');
+const pkg = require('../../package.json');
 
 router.get('/', function (req, res) {
-    const data = { version: package.version };
+    const data = { version: pkg.version };
     res.json(data);
 });
 router.use('/strava', stravaRoute);
