02ac0b6e4533aca97b1c706873c4d03b265565b7
Patrick Connelly <patrick@deadlypenguin.com>
2021-02-01 20:54:00 -0500

    Refactoring node module asset handling for cleaner main file

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const path = require('path');
+const lodash = require('lodash');
 const passport = require('passport');
 const StravaStrategy = require('passport-strava').Strategy;
+
 const passportUtils = require('./utils/passport');
+const routingUtils = require('./utils/routing');
 
 const indexRouter = require('./routes/index');
 const myRouter = require('./routes/my');
@@ -16,60 +19,7 @@ passport.deserializeUser(passportUtils.deserializeUser);
 
 app.set('view engine', 'ejs');
 
-app.get('/css/bulma.min.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/bulma/css/bulma.min.css'));
-});
-app.get('/js/vue.min.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/vue/dist/vue.min.js'));
-});
-app.get('/js/axios.min.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/axios/dist/axios.min.js'));
-});
-app.get('/js/moment.min.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/moment/min/moment.min.js'));
-});
-app.get('/js/jquery.slim.min.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/jquery/dist/jquery.slim.min.js'));
-});
-app.get('/css/fontawesome.min.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css'));
-});
-app.get('/css/solid.min.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/css/solid.min.css'));
-});
-app.get('/css/brands.min.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/css/brands.min.css'));
-});
-app.get('/webfonts/fa-solid-900.woff', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff'));
-});
-app.get('/webfonts/fa-solid-900.woff2', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2'));
-});
-app.get('/webfonts/fa-solid-900.ttf', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf'));
-});
-app.get('/webfonts/fa-solid-900.eot', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.eot'));
-});
-app.get('/webfonts/fa-solid-900.svg', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg'));
-});
-app.get('/webfonts/fa-brands-400.woff', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff'));
-});
-app.get('/webfonts/fa-brands-400.woff2', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2'));
-});
-app.get('/webfonts/fa-brands-400.ttf', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf'));
-});
-app.get('/webfonts/fa-brands-400.eot', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.eot'));
-});
-app.get('/webfonts/fa-brands-400.svg', function (req, res) {
-    res.sendFile(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.svg'));
-});
+app.get(lodash.keys(routingUtils.module_assets), routingUtils.getAsset);
 
 const session_data = {
     secret: process.env.SESSION_SECRET,
@@ -82,38 +32,12 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/auth/strava', passport.authenticate('strava', passportUtils.strava.authConfig));
-app.get('/auth/strava/callback', passport.authenticate('strava', passportUtils.strava.callbackConfig), function (req, res) {
-    let url = '/';
-
-    if (req.session && req.session.origin) {
-        url = req.session.origin;
-    }
-
-    if (req.session) {
-        delete req.session.origin;
-    }
-
-    res.redirect(url);
-});
+app.get('/auth/strava/callback', passport.authenticate('strava', passportUtils.strava.callbackConfig), routingUtils.postAuthRedirect);
 
 app.use('/', indexRouter);
 app.use('/my', myRouter);
 app.use('/api', apiRouter);
 
-app.use(function (req, res){
-    res.status(404);
-
-    if (req.accepts('html')) {
-        res.render('pages/error', { url: req.url });
-        return;
-    }
-
-    if (req.accepts('json')) {
-        res.send({ error: 'Not found' });
-        return;
-    }
-
-    res.type('txt').send('Not found');
-});
+app.use(routingUtils.error);
 
 module.exports = app;
--- utils/routing.js
+++ utils/routing.js
@@ -1,3 +1,44 @@
+const path = require('path');
+const lodash = require('lodash');
+
+const module_assets = {
+    '/css/bulma.min.css': '/node_modules/bulma/css/bulma.min.css',
+    '/js/vue.min.js': '/node_modules/vue/dist/vue.min.js',
+    '/js/axios.min.js': '/node_modules/axios/dist/axios.min.js',
+    '/js/moment.min.js': '/node_modules/moment/min/moment.min.js',
+    '/js/jquery.slim.min.js': '/node_modules/jquery/dist/jquery.slim.min.js',
+    '/css/fontawesome.min.css': '/node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
+    '/css/solid.min.css': '/node_modules/@fortawesome/fontawesome-free/css/solid.min.css',
+    '/css/brands.min.css': '/node_modules/@fortawesome/fontawesome-free/css/brands.min.css',
+    '/webfonts/fa-solid-900.woff': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff',
+    '/webfonts/fa-solid-900.woff2': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2',
+    '/webfonts/fa-solid-900.ttf': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf',
+    '/webfonts/fa-solid-900.eot': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.eot',
+    '/webfonts/fa-solid-900.svg': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg',
+    '/webfonts/fa-brands-400.woff': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff',
+    '/webfonts/fa-brands-400.woff2': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2',
+    '/webfonts/fa-brands-400.ttf': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf',
+    '/webfonts/fa-brands-400.eot': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.eot',
+    '/webfonts/fa-brands-400.svg': '/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.svg'
+};
+
+/**
+ * Gets the asset from the node modules
+ * @param {Object} req The request
+ * @param {Object} res The response
+ * @param {Object} next The next method
+ * @returns {undefined}
+ */
+function getAsset(req, res, next) {
+    const url = req.url;
+
+    if (lodash.has(module_assets, url)) {
+        res.sendFile(path.join(__dirname, '..', lodash.get(module_assets, url)));
+    } else {
+        next();
+    }
+}
+
 /**
  * Verifies that we have a user already
  * @param {Object} req The request
@@ -15,4 +56,52 @@ function verifyAuth(req, res, next) {
     res.redirect('/login');
 }
 
-module.exports = { verifyAuth: verifyAuth };
+/**
+ * Returns an error if the request is unknown
+ * @param {Object} req The request
+ * @param {Object} res The response
+ * @returns {undefined}
+ */
+function error(req, res) {
+    res.status(404);
+
+    if (req.accepts('html')) {
+        res.render('pages/error', { url: req.url });
+        return;
+    }
+
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    res.type('txt').send('Not found');
+}
+
+/**
+ * Redirects to the original page after login if requested
+ * @param {Object} req The request
+ * @param {Object} res The response
+ * @returns {undefined}
+ */
+function postAuthRedirect(req, res) {
+    let url = '/';
+
+    if (req.session && req.session.origin) {
+        url = req.session.origin;
+    }
+
+    if (req.session) {
+        delete req.session.origin;
+    }
+
+    res.redirect(url);
+}
+
+module.exports = {
+    error: error,
+    getAsset: getAsset,
+    module_assets: module_assets,
+    postAuthRedirect: postAuthRedirect,
+    verifyAuth: verifyAuth
+};
