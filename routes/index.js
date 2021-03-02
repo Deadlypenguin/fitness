d2d02c6bc58e2f0a9365c11c6987283155fed074
Patrick Connelly <patrick@deadlypenguin.com>
2021-03-02 16:55:30 -0500

    Adding logout page

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,9 @@ router.get('/login', function (req, res) {
     res.render('pages/login');
 });
 
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
 module.exports = router;
